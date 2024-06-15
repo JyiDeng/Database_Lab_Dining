378ec32539abee2fd747d4384512baa050e70d0d
WuSuting <22302010087@m.fudan.edu.cn>
2024-06-15 13:32:30 +0800

    feat:favorite拆成两个实体类

---
+++ src/main/java/com/example/pj/entity/UserFavoriteDish.java
@@ -0,0 +1,19 @@
+package com.example.pj.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+@Component
+public class UserFavoriteDish {
+    private Long userId;
+    private Long dishId;
+    private String favoriteDate;
+
+}
